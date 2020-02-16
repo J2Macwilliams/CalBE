8a24d648e11ec98c713734f18b750086d53e5764
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-16 08:20:18 -0800

    Config .env.

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const authRouter = require('../routes/authRouter');
 
-router.use('/api', authRouter);
+router.use('/api/auth', authRouter);
 
 // Global test endpoint
 router.get('/', (req, res) => {
--- api/server.js
+++ api/server.js
@@ -1,10 +1,27 @@
-const express = require('express');
+//Require env variables
+require('dotenv').config();
 
 // Require
+const express = require('express');
+const session = require('express-session'); 
 const apiRouter = require('./apiRouter.js');
 const configureMiddleware = require('./configMiddleware.js');
+// Config session and store
+const sessionConfig = {
+	name: 'sid',
+	saveUninitialized: false,
+	resave: false,
+	secret: process.env.SESSION_SECRET,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 2,
+		secure: process.env.NODE_ENV === 'production' ? true : false,
+		sameSite: false,
+		httpOnly: true
+	}
+}
 
 const server = express();
+server.use(session(sessionConfig));
 
 configureMiddleware(server);
 
