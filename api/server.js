2989dab35abb43d39cd5619fa29147b21b9cdff4
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-20 20:36:03 -0800

    Fix server.js

--- api/server.js
+++ api/server.js
@@ -1,36 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
-const authRoute = require('../routes/auth');
-// const eventsRoute = require('../routes/events');
-// const { db } = require('../db');
+const profileRoute = require("../routes/profile");
+const eventsRoute = require("../routes/events");
 
 //Require env variables
-require('dotenv').config();
 const server = express();
 
-let corsOptions = {
-  origin: [process.env.FRONTEND_URL, process.env.STAGING_FRONTEND_URL, process.env.PRODUCTION_FRONTEND_URL ],
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
-};
-
 server.use(helmet());
-server.use(cors(corsOptions));
+server.use(cors());
 server.use(express.json());
-// server.use(
-//   session({
-//     name: 'sid',
-//     saveUninitialized: false,
-//     resave: false,
-//     secret: process.env.SESSION_SECRET,
-//     store: new MongoStore({ mongooseConnection: db })
-//   })
-// );
 
-server.use('/api/auth', authRoute);
-// server.use('/api/events', eventsRoute);
+server.use("/api/profile", profileRoute);
+server.use("/api/events", eventsRoute);
 
 server.get('/', (req, res) => {
   res.send({ api: 'Ok', dbenv: process.env.DB_ENV });
